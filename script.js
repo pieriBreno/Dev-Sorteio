2863911f07280997c376fcf03873c1754a854e30
pieriBreno <pieribreno@gmail.com>
2023-12-06 21:14:52 -0300

    Incrementação regra de mercado

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function generateNumber() { 
+function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-   const result = Math.floor(Math.random() * (max - min +1)) + min;
+    if (min > max) {
+        alert("O valor mínimo deve ser MAIOR que o valor máximo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+        alert(result)
+    }
 }
